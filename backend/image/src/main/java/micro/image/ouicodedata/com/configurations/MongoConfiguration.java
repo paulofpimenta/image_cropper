60f5478465e1c5da19ebe12c1e73feab4962a68f
Paulo Pimenta <pfpimenta@gmail.com>
2024-06-23 21:50:45 +0200

    Delete unsed code

--- backend/image/src/main/java/micro/image/ouicodedata/com/configurations/MongoConfiguration.java
+++ backend/image/src/main/java/micro/image/ouicodedata/com/configurations/MongoConfiguration.java
@@ -1,34 +1,11 @@
 package micro.image.ouicodedata.com.configurations;
 
-import com.mongodb.client.ListDatabasesIterable;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import lombok.RequiredArgsConstructor;
-import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-
-import java.util.Objects;
 
 @Profile("prod")
 @Configuration
 public class MongoConfiguration  {
-
-    @Autowired
-    private Environment env;
-
-//    @Value("${spring.data.mongodb.uri}")
-//    private String uri;
-//
-//    @Value("${spring.data.mongodb.database}")
-//    private String db;
 
 
 }
